9682672c3c61ea76a21d0be7f32a267d6c2e98c8
Sidney de Vries <sidney.de.vries96@gmail.com>
2022-06-15 22:29:10 +0200

    Initial commit

---
+++ api_call.py
@@ -0,0 +1,23 @@
+from base64 import b64decode
+import requests
+import time
+
+
+def call_dalle_api(query):
+    """ Calls dalle api with passed query."""
+    # Because api could be overloaded, we use a while loop to make the request
+    # If we get a status code 200 response we break the loop, otherwise we wait 2 seconds
+    # and try again
+    while True:
+        print("Making request...")
+        response = requests.post("https://bf.dallemini.ai/generate", json={"prompt": query})
+        if response.status_code == 200:
+            # Turn raw response into dictionary
+            response = response.json()
+            break
+        else:
+            print(f"Request returned status code {response.status_code}, trying again in 2 seconds")
+            time.sleep(2)
+
+    # Nine base64 decoded images in a list
+    return [b64decode(img) for img in response["images"]]
---
+++ prompt_model.py
@@ -0,0 +1,26 @@
+import markovify
+
+
+class PromptModel:
+    def __init__(self):
+        self.model = None
+
+    def fit_model(self, corpus_path, state_size=3):
+        """ """
+        corpus = self.load_data(corpus_path)
+
+        self.model = markovify.NewlineText(corpus, state_size=state_size)
+        self.model = self.model.compile()
+
+    def generate_prompt(self, n=1):
+        """ Generate n random prompts. """
+        # Need a high number for tries to prevent returning Nones
+        return [self.model.make_sentence(tries=999) for _ in range(n)]
+
+    @staticmethod
+    def load_data(corpus_path):
+        """ Loads .txt file with every sentence on a separate line to string. """
+        with open(corpus_path, "r", encoding="utf8") as f:
+            corpus = f.read()
+
+        return corpus
